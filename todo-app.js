ba6709c59e4e5d06280d6df24b0555d83ff6345f
LauraNicole449 <lauranicolegarcia449@gmail.com>
2023-12-24 22:45:49 +0100

    change color task list

--- todo-app.js
+++ todo-app.js
@@ -1,22 +1,62 @@
+var taskId = 1;
 function addTodoList(){
+    
+    const inputValue = document.getElementById("createTodo").value;
+    const newListElement = document.createElement("li");
+    const textNode = document.createTextNode(inputValue);
+    const imgElement = document.createElement("img");
+    const divElement = document.createElement("div");
+    const todoList = document.getElementById("listTodo");
     if (event.key === "Enter") {
-        const inputValue = document.getElementById("createTodo").value;
-        const newListElement = document.createElement("li");
-        const textNode = document.createTextNode(inputValue);
+        
         if(inputValue === ""){
             return inputValue;
         }else{
-            const imgElement = document.createElement("img");
+            
+            
             imgElement.src = "images/icon-check.svg"; 
-            newListElement.appendChild(imgElement);
-            const textNode = document.createTextNode(inputValue);
+          //IMAGE CHECK ELEMENT
+            newListElement.appendChild(divElement).style.width = "10px";
+            newListElement.appendChild(divElement).style.height = "10px";
+            newListElement.appendChild(divElement).style.marginRight = "10px";
+            newListElement.appendChild(divElement).style.backgroundColor = "#a07ef5";
+            
+            newListElement.appendChild(divElement).style.borderRadius = "100%";
+            
+            
+           
+           
+            //TEXT
+            
+            //LIST ELEMENTS
             newListElement.appendChild(textNode);
-            const todoList = document.getElementById("listTodo");
+            
+            todoList.appendChild(newListElement).style.padding = "25px";
+            todoList.appendChild(newListElement).style.borderBottom = "solid";
+            todoList.appendChild(newListElement).style.display = "flex";
+            todoList.appendChild(newListElement).style.backgroundColor = "red";
+            todoList.appendChild(newListElement).style.borderBottomWidth = "1px";
+            todoList.appendChild(newListElement).style.borderBottomColor = "white";
+            todoList.appendChild(newListElement).setAttribute("id", taskId);
+            todoList.appendChild(newListElement).onclick = completeTask;
+
             todoList.appendChild(newListElement);
+
             document.getElementById("createTodo").value = "";
         }
+        console.log(taskId);
+        taskId = taskId+1;
         
         console.log("assa");
     }
+    
 }
 
+
+ function completeTask(elemento) {
+      if (elemento.style.backgroundColor === "red") {
+        elemento.style.backgroundColor = "blue";
+      } else {
+        elemento.style.backgroundColor = "red";
+      }
+    }
