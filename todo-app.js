049bbb1626f7ac80eeb162884ff1fb5de1838757
LauraNicole449 <lauranicolegarcia449@gmail.com>
2023-12-28 19:52:21 +0100

    idk

--- todo-app.js
+++ todo-app.js
@@ -4,31 +4,31 @@ function addTodoList(){
     const inputValue = document.getElementById("createTodo").value;
     const newListElement = document.createElement("li");
     const textNode = document.createTextNode(inputValue);
-    const imgElement = document.createElement("img");
-    const divElement = document.createElement("div");
+    const imgCross = document.createElement("img");
+    const checkElement = document.createElement("div");
     const todoList = document.getElementById("listTodo");
     if (event.key === "Enter") {
         
         if(inputValue === ""){
             return inputValue;
         }else{
-          newListElement.appendChild(divElement);
+          
             
-            imgElement.src = "images/icon-check.svg"; 
+            imgCross.src = "images/icon-cross.svg"; 
           //IMAGE CHECK ELEMENT
-            newListElement.appendChild(divElement).style.width = "15px";
-            newListElement.appendChild(divElement).style.height = "15px";
-            newListElement.appendChild(divElement).style.marginRight = "10px";
-            newListElement.appendChild(divElement).style.backgroundColor = "red";
-            newListElement.appendChild(divElement).style.borderWidth = "40px"
-            newListElement.appendChild(divElement).style.borderColor = "#47495e";
+            newListElement.appendChild(checkElement).style.width = "15px";
+            newListElement.appendChild(checkElement).style.height = "15px";
+            newListElement.appendChild(checkElement).style.marginRight = "10px";
+            newListElement.appendChild(checkElement).style.borderRadius = "100%";
+            newListElement.appendChild(checkElement).style.backgroundColor = "#25273c";
+            newListElement.appendChild(checkElement).style.borderWidth = "0.5px"
+            newListElement.appendChild(checkElement).style.borderColor = "#47495e";
+            newListElement.appendChild(checkElement).style.borderStyle = "solid";
+
             
             
             
             
-           
-           
-            //TEXT
             
             //LIST ELEMENTS
             newListElement.appendChild(textNode);
@@ -42,30 +42,50 @@ function addTodoList(){
             todoList.appendChild(newListElement).setAttribute("id", taskId);
             todoList.appendChild(newListElement).onclick = completeTask;
 
-            todoList.appendChild(newListElement);
+            //IMAGE CROSS
+            newListElement.appendChild(imgCross);
+            newListElement.appendChild(imgCross).onclick = deleteTask;
+
+
 
             document.getElementById("createTodo").value = "";
         }
         console.log(taskId);
         taskId = taskId+1;
-        
+        function completeTask() {
+          if(!this.dataset.completed){
+            
+            
+            this.style.textDecoration = "line-through";
+            this.style.color = "#47495e";
+            this.appendChild(checkElement).style.backgroundColor = "white";
+            this.appendChild(textNode);
+            newListElement.appendChild(imgCross);
+            this.dataset.completed = true;
+          } else {
+            
+            this.appendChild(textNode);this.style.textDecoration = "none";
+            this.style.color = "white";
+            this.appendChild(checkElement).style.backgroundColor = "#25273c";
+            this.appendChild(textNode);
+            newListElement.appendChild(imgCross);
+            delete this.dataset.completed;
+          }
+        }
+
+        function deleteTask(){
+          var idList = event.target.id;
+          document.getElementById(idList).remove();
+          console.log("imagen");
+          
+        }
+
         console.log("assa");
     }
   }
 
- function completeTask() {
-  if(!this.dataset.completed){
-    this.style.textDecoration = "line-through";
-    this.style.color = "#47495e";
-    this.dataset.completed = true;
-  } else {
-    this.style.textDecoration = "none";
-    this.style.color = "white";
-    delete this.dataset.completed;
-  }
-}
 
-    
+ 
 //RESERVE THIS CODE
   /*  if (toggleColor === 1) {
       this.style.textDecoration = "line-through";
